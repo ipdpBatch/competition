aa56cf05dcb5f229523f3a59293962ae6c1e311c
fatcatfish <zpeng9532@yahoo.com>
2019-09-25 20:43:26 +0800

    fix

--- OctopusParent/UserServiceProvider/src/test/java/com/octopus/eureka/UserServiceApplicationTests.java
+++ OctopusParent/UserServiceProvider/src/test/java/com/octopus/eureka/UserServiceApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EurekaApplicationTests {
+public class UserServiceApplicationTests {
 
     @Autowired
     private UserMapper userMapper;
