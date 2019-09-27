c3fd90920695fe53a147381f6fa1d36f796df816
fatcatfish <zpeng9532@yahoo.com>
2019-09-27 20:42:20 +0800

    add mq interface

---
+++ OctopusParent/ControlCenture/src/main/java/com/octopus/eureka/control/IMqSender.java
@@ -0,0 +1,12 @@
+package com.octopus.eureka.control;
+
+/**
+ * 文件创建时写入注释内容
+ *
+ * @author zp3778
+ * @version 1.0.0
+ * @date Created in 20:34 2019/9/27
+ */
+interface IMqSender {
+     int sendBuyMessage(BuyBo buyBo);
+}
--- OctopusParent/ControlCenture/src/main/java/com/octopus/eureka/control/OctopusEarController.java
+++ OctopusParent/ControlCenture/src/main/java/com/octopus/eureka/control/OctopusEarController.java
@@ -2,8 +2,12 @@ package com.octopus.eureka.control;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Map;
 
@@ -12,6 +16,10 @@ import java.util.Map;
 @RequestMapping("/octopus")
 public class OctopusEarController {
     private final static Logger logger = LoggerFactory.getLogger(OctopusEarController.class);
+
+    @Autowired
+    private IMqSender mqSender;
+
 
     /*@GetMapping(value = "/customerId/{customerId}/productId/{productId}/transactionAmount/{transactionAmount}")
     public String buyRequest(@PathVariable("customerId") String id, @PathVariable("productId") String pid, @PathVariable("transactionAmount") BigDecimal txnamt) {
@@ -24,16 +32,6 @@ public class OctopusEarController {
         return new Date();
     }
 
-    /**
-     * 带有 URI 模板
-     */
-/*
-    @GetMapping(path = "/{day}")
-    public String getForDay(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) Date day, Model model) {
-        System.out.println("--> " + new SimpleDateFormat("yyyy-MM-dd").format(day));
-        return "-->" + new SimpleDateFormat("yyyy-MM-dd").format(day);
-    }
-*/
     @GetMapping("/post")
     public Map<String, String> test(@RequestParam Map<String, String> fieldValueList) {
         return fieldValueList;
@@ -42,5 +40,13 @@ public class OctopusEarController {
     @GetMapping("/posta")
     public String post(@RequestParam String username, @RequestParam String password) {
         return "Username:" + username + "'\n'password:" + password;
+    }
+
+    @GetMapping("/buy")
+    public int buy(@RequestParam String businessCode, @RequestParam String customerId, @RequestParam String productId, @RequestParam BigDecimal transactionAmount) {
+            //return "businessCode:" + businessCode + "\ncustomerId:" + customerId +"\nproductId:"+productId +"\ntransactionAmount:"+ transactionAmount ;
+      BuyBo buyBo = new BuyBo(businessCode,customerId,productId,transactionAmount);
+      int res= mqSender.sendBuyMessage(buyBo);
+      return res;
     }
 }
