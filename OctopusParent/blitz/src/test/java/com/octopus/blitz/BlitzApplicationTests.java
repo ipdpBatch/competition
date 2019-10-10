a2b2877861ec3ca3412afdd37a929197f8741d09
Tian Sheng <sheng.tian.china@gmail.com>
2019-10-10 20:07:13 +0800

    修改秒杀模块blitz。

--- OctopusParent/blitz/src/test/java/com/octopus/blitz/BlitzApplicationTests.java
+++ OctopusParent/blitz/src/test/java/com/octopus/blitz/BlitzApplicationTests.java
@@ -1,16 +1,18 @@
 package com.octopus.blitz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlitzApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		log.info("HELLO WORLD!");
 	}
 
 }
