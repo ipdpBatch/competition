c2b3913f1e94ddd8a24be2e68e66b2b2df564014
smurf669 <liuyn317@163.com>
2019-10-10 13:59:09 +0800

    产品检查

--- OctopusParent/FeignClient/src/main/java/com/octopus/feign/consumer/ProductDispatcher.java
+++ OctopusParent/FeignClient/src/main/java/com/octopus/feign/consumer/ProductDispatcher.java
@@ -1,14 +1,17 @@
 package com.octopus.feign.consumer;
 
 import com.octopus.common.bo.BuyBo;
+import com.octopus.common.bo.BuyResponseBo;
 import com.octopus.common.dao.domain.ControlProductDto;
 import com.octopus.common.dao.domain.ProductBaseInfoDto;
+import com.octopus.eureka.product.ProductService;
 import com.octopus.feign.consumer.provider.ProductClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Date;
@@ -24,8 +27,10 @@ public class ProductDispatcher {
     private final static Logger logger = LoggerFactory.getLogger(ProductDispatcher.class);
 //    @Autowired
 //    private HomeClient homeClient;
+
     @Autowired
     private ProductClient productClient;
+
 
     //产品控制表
     public List<ControlProductDto> getControlProductList(){
@@ -64,51 +69,8 @@ public class ProductDispatcher {
         return productClient.insertProduct(productBaseInfoDto);
     }
 
-    /*产品检查*/
-    public Map checkProduct(BuyBo buyBo) {
-        Map result=new HashMap();
-        //产品控制表记录信息
-        ControlProductDto controlProductDto = new ControlProductDto();
-        controlProductDto.setOrderSeq(buyBo.getOrderSeq());
-        controlProductDto.setOrderStep(buyBo.getOrderStep());
-        String requestTime=formatTime(new Date());
-        controlProductDto.setRequestTime(requestTime);
-        controlProductDto.setUpdateTime(formatTime(new Date()));
-        controlProductDto.setStepStatus("INIT");
-        int i=productClient.addControlProduct(controlProductDto);
-        if(i!=1){
-            result.put("checkResult","false");
-            result.put("reason","插入产品控制表失败");
-        }else{
-            ProductBaseInfoDto productBaseInfoDto = productClient.findById(buyBo.getProductId());
-            logger.info("产品信息为："+productBaseInfoDto.toString());
-            if(productBaseInfoDto==null){
-                result.put("checkResult","false");
-                result.put("reason","无此产品");
-
-                controlProductDto.setUpdateTime(formatTime(new Date()));
-                controlProductDto.setStepStatus("PCFL");
-                int j=productClient.updateControlProduct(controlProductDto);
-
-            }else{
-                BigDecimal raiseAmount=productBaseInfoDto.getProductRaiseAmount();
-                if(raiseAmount.compareTo(buyBo.getTransactionAmount())<0){//额度不足
-                    result.put("checkResult","false");
-                    result.put("reason","产品额度不足");
-
-                    controlProductDto.setUpdateTime(formatTime(new Date()));
-                    controlProductDto.setStepStatus("PCFL");
-                    int j=productClient.updateControlProduct(controlProductDto);
-                } else{
-                    result.put("checkResult","true");
-                    result.put("reason","检查通过");
-
-                    controlProductDto.setUpdateTime(formatTime(new Date()));
-                    controlProductDto.setStepStatus("PCSC");
-                    int j=productClient.updateControlProduct(controlProductDto);
-                }
-            }
-        }
-        return result;
+    //产品检查
+    public BuyResponseBo checkProduct(BuyBo buyBo) {
+        return productClient.checkProduct(buyBo);
     }
 }
---
+++ OctopusParent/ProductServiceProvider/src/main/java/com/octopus/eureka/product/ProductService.java
@@ -0,0 +1,90 @@
+package com.octopus.eureka.product;
+
+import com.octopus.common.bo.BuyBo;
+import com.octopus.common.bo.BuyResponseBo;
+import com.octopus.common.dao.domain.ControlProductDto;
+import com.octopus.common.dao.domain.ProductBaseInfoDto;
+import com.octopus.common.dao.mapper.ProductBaseInfoMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import static com.octopus.common.utils.DateUtil.formatTime;
+
+/**
+ *
+ * @author lyn
+ * @version 1.0.0
+ * @date 2019-10-9
+ */
+@RestController
+public class ProductService {
+    private final static Logger logger = LoggerFactory.getLogger(ProductService.class);
+
+    @Resource
+    private ProductBaseInfoController productBaseInfoController;
+
+    @Resource
+    private ProductController productController;
+
+    /*产品检查*/
+    public BuyResponseBo checkProduct(BuyBo buyBo) {
+        BuyResponseBo buyResponseBo=new BuyResponseBo();
+        buyResponseBo.setOrderSeq(buyBo.getOrderSeq());
+        buyResponseBo.setBusinessCode(buyBo.getBusinessCode());
+        buyResponseBo.setCustomerId(buyBo.getCustomerId());
+        buyResponseBo.setProductId(buyBo.getProductId());
+        buyResponseBo.setTransactionAmount(buyBo.getTransactionAmount());
+        buyResponseBo.setDestiny(buyBo.getDestiny());
+        buyResponseBo.setOrderStep(buyBo.getOrderStep());
+        //产品控制表记录信息
+        ControlProductDto controlProductDto = new ControlProductDto();
+        controlProductDto.setOrderSeq(buyBo.getOrderSeq());
+        controlProductDto.setOrderStep(buyBo.getOrderStep());
+        String requestTime=formatTime(new Date());
+        controlProductDto.setRequestTime(requestTime);
+        controlProductDto.setUpdateTime(formatTime(new Date()));
+        controlProductDto.setStepStatus("INIT");
+        int i=productController.addControlProduct(controlProductDto);
+        if(i!=1){
+            buyResponseBo.setOrderReturnCode(false);
+            buyResponseBo.setErrorDetail("插入产品控制表失败");
+        }else{
+            ProductBaseInfoDto productBaseInfoDto = productBaseInfoController.findById(buyBo.getProductId());
+            logger.info("产品信息为："+productBaseInfoDto.toString());
+            if(productBaseInfoDto==null){
+                buyResponseBo.setOrderReturnCode(false);
+                buyResponseBo.setErrorDetail("无此产品");
+
+                controlProductDto.setUpdateTime(formatTime(new Date()));
+                controlProductDto.setStepStatus("PCFL");
+                int j=productController.updateControlProduct(controlProductDto);
+
+            }else{
+                BigDecimal raiseAmount=productBaseInfoDto.getProductRaiseAmount();
+                if(raiseAmount.compareTo(buyBo.getTransactionAmount())<0){//额度不足
+                    buyResponseBo.setOrderReturnCode(false);
+                    buyResponseBo.setErrorDetail("产品额度不足");
+
+                    controlProductDto.setUpdateTime(formatTime(new Date()));
+                    controlProductDto.setStepStatus("PCFL");
+                    int j=productController.updateControlProduct(controlProductDto);
+                } else{
+                    buyResponseBo.setOrderReturnCode(true);
+                    buyResponseBo.setErrorDetail("检查通过");
+
+                    controlProductDto.setUpdateTime(formatTime(new Date()));
+                    controlProductDto.setStepStatus("PCSC");
+                    int j=productController.updateControlProduct(controlProductDto);
+                }
+            }
+        }
+        return buyResponseBo;
+    }
+}
